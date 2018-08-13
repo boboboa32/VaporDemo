7356a5a4dd249e728ba0e46d81db9506c162e8ad
bobo <bobo@xiaoman.cn>
2018-08-13 11:42:26 +0800

    add hello get router

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,9 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
+    router.get("hello") { req -> String in
+        return "Hellow, world"
+    }
     
     router.post("api", "acronyms") { req -> Future<Acronym> in
         return try req.content.decode(Acronym.self).flatMap(to: Acronym.self) { acronym in
